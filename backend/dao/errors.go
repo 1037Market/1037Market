a8f3f9c147088312b545a6d7e95f489be9622646
emptyset <63343750+emptysetvvvv@users.noreply.github.com>
2023-11-05 13:12:53 +0800

    Add error handle

---
+++ backend/dao/errors.go
@@ -0,0 +1,25 @@
+package dao
+
+type ErrorTypeDao int
+
+const (
+	ErrTypeDatabaseConnection ErrorTypeDao = iota + 1
+	ErrTypeDatabaseQuery
+	ErrTypeDatabaseScanRows
+)
+
+type ErrorDao struct {
+	Type    ErrorTypeDao
+	Message string
+}
+
+func (e *ErrorDao) Error() string {
+	return e.Message
+}
+
+func NewErrorDao(errType ErrorTypeDao, message string) *ErrorDao {
+	return &ErrorDao{
+		Type:    errType,
+		Message: message,
+	}
+}
---
+++ backend/server/api/handle.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"1037Market/dao"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func handleError(c *gin.Context, err error) {
+	if err == nil {
+		return
+	}
+
+	errorDao, ok := err.(*dao.ErrorDao)
+	if !ok {
+		log.Println("unknown error", err.Error())
+		c.String(http.StatusInternalServerError, "internal error")
+		return
+	}
+
+	switch errorDao.Type {
+	case dao.ErrTypeDatabaseConnection:
+		log.Print("database connection error", errorDao.Message)
+		c.String(http.StatusInternalServerError, "database error")
+	case dao.ErrTypeDatabaseQuery:
+		log.Println("database query error", errorDao.Message)
+		c.String(http.StatusInternalServerError, "database error")
+	case dao.ErrTypeDatabaseScanRows:
+		log.Println("database scan error", errorDao.Message)
+		c.String(http.StatusInternalServerError, "database error")
+	}
+
+}
