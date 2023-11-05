0037be6b778705a23a7293fe2eca29d1f4fc3f7f
uniqueFranky <y_r_b@qq.com>
2023-11-05 22:51:27 +0800

    update chat conceptions

--- backend/server/api/chat.go
+++ backend/server/api/chat.go
@@ -8,7 +8,7 @@ import (
 
 /*
 TODO:
-chat API
+chat实现思路
 
 新建两张表：CHAT_SESSIONS CHAT_MESSAGES
 
@@ -17,6 +17,20 @@ CHAT_SESSIONS: user1Id和user2Id需要保证 user1Id <= user2Id，这是需要se
 CHAT_MESSAGES: 每个message属于一个session。
 				type暂时分为文字和图片两种，图片存URI，文字直接存，后端不需要过多处理，只需要解析前端传来的类型
 				需要支持在固定sessionId的情况下根据messageId的排序查询，因为前端需要查询从倒数第i条开始的n条消息
+*/
+
+/*
+TODO:
+需要的API
+	根据用户ID查询SessionID 即CHAT_SESSIONS表中user1Id或user2Id == studentID 的SessionID
+
+	根据sessionID返回参与聊天的两个人的信息
+
+	根据sessionID查询该session中最近一次的messageID
+
+	查询同一session中的从 倒数第i条开始的n条消息的messageID
+
+	根据messageID返回message信息
 
 */
 
